3c9f964ee9b380d3980f4a01428bd32f52e0e3c2
Blurgy <gaoyangzhang@outlook.com>
2019-06-16 21:31:10 +0800

    random update

--- DIP/mnist/exe/1.py
+++ DIP/mnist/exe/1.py
@@ -7,29 +7,62 @@ import pickle
 import numpy as np 
 from PIL import Image 
 # import torch
+# from torch.autograd import Variable 
 
-traning_path = "dat/training";
-traning_img = [];
+training_set_dmp_fname = "../dmp/training_set.pickle";
+# testing_set_dmp_fname = "../dmp/testing_set.pickle";
+training_imgs = [];
+# testing_img = [];
 
-print("reading pickle file..")
-with open("traning_set.pickle", 'rb') as f:
-    traning_img = pickle.load(f);
-print("pickle file read in memory");
+with open(training_set_dmp_fname, 'rb') as f:
+    training_imgs = pickle.load(f);
+print("training set loaded")
 
+w = np.zeros(7840).reshape(10, 784);
+print(np.log(np.exp(1)));
 
-# cnt = 0;
-# for root, dirs, files in os.walk(traning_path):
-#     for file in files:
-#         if(file.split('.')[-1] == "png"):
-#             img = Image.open(root + '/' + file);
-#             arr = np.array(img);
-#             traning_img.append(arr.reshape(-1, 1));
-#             cnt += 1;
-#             if(cnt % 1000 == 0):
-#                 print(cnt);
-# print("traning_img in memory");
+def regularization_loss(w, ):
+    try:
+        rows, cols = w.shape;
+        ret = 0;
+        for i in range(rows):
+            for j in range(cols):
+                ret += w[i][j] ** 2;
+        return ret;
+    except Exception as e:
+        print("regularization_loss(): %s" % e);
+        return None;
 
-# print("dumping..");
-# with open("traning_set.pickle", 'wb') as f:
-#     pickle.dump(traning_img, f);
+def softmax_loss(score, gt, ):
+    try:
+        ret = 0;
+        smax = score;
+        rows, cols = smax.shape;
+        sum = 0;
+        for i in cols:
+            smax[0][i] = np.exp(score[0][i]);
+            sum += smax[0][i];
+        ret = -np.log(smax[0][gt]/sum);
+        return ret;
+    except Exception as e:
+        print("softmax_loss(): %s" % e);
+        return None;
 
+def loss(w, score, ground_truth):
+    try:
+        return softmax_loss(score, ground_truth) + regularization_loss(w);
+    except Exception as e:
+        print("loss(): %s" % e);
+        return None;
+
+def train(training_imgs, w, ):
+    try:
+        size = len(training_imgs);
+        for i in range(size):
+            score = np.dot(w, training_imgs[i][0]);
+            print(score, training_imgs[i][1]);
+        return w;
+    except Exception as e:
+        print("train(): %s" % e);
+
+train(training_imgs, w);
