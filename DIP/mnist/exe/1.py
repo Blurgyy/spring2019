a9958fc67a0ef4e177386e08b0a1c3342264384b
root <gaoyangzhang@outlook.com>
2019-06-17 09:04:56 +0800

    linear classifier (precision converged at 84%)

--- DIP/mnist/exe/1.py
+++ DIP/mnist/exe/1.py
@@ -3,9 +3,11 @@
 __author__ = "zgy";
 
 import os 
+import sys
 import pickle 
 import numpy as np 
 from PIL import Image 
+import random 
 # import torch
 # from torch.autograd import Variable 
 
@@ -13,13 +15,19 @@ training_set_dmp_fname = "../dmp/training_set.pickle";
 # testing_set_dmp_fname = "../dmp/testing_set.pickle";
 training_imgs = [];
 # testing_img = [];
+w_dmp_fname = "../dmp/w.pickle";
 
 with open(training_set_dmp_fname, 'rb') as f:
     training_imgs = pickle.load(f);
-print("training set loaded")
-
-w = np.zeros(7840).reshape(10, 784);
-print(np.log(np.exp(1)));
+print("training set loaded");
+retrain = False;
+if(len(sys.argv) > 1 and sys.argv[1] == "--retrain"):
+    retrain = True;
+if(os.path.exists(w_dmp_fname) and not retrain):
+    with open(w_dmp_fname, 'rb') as f:
+        w = pickle.load(f);
+else:
+    w = np.zeros(7840).reshape(10, 784);
 
 def regularization_loss(w, ):
     try:
@@ -39,30 +47,78 @@ def softmax_loss(score, gt, ):
         smax = score;
         rows, cols = smax.shape;
         sum = 0;
-        for i in cols:
-            smax[0][i] = np.exp(score[0][i]);
-            sum += smax[0][i];
-        ret = -np.log(smax[0][gt]/sum);
+        for i in range(rows):
+            smax[i][0] = np.exp(score[i][0]);
+            sum += smax[i][0];
+        ret = -np.log(smax[gt][0]/sum);
         return ret;
     except Exception as e:
         print("softmax_loss(): %s" % e);
         return None;
 
-def loss(w, score, ground_truth):
+def total_loss(w, score, ground_truth, Lambda, ):
     try:
-        return softmax_loss(score, ground_truth) + regularization_loss(w);
+        sloss = softmax_loss(score, ground_truth);
+        rloss = regularization_loss(w);
+        # print(sloss, rloss);
+        return sloss + Lambda * rloss;
     except Exception as e:
-        print("loss(): %s" % e);
+        print("total_loss(): %s" % e);
         return None;
 
-def train(training_imgs, w, ):
+def update(w, x, loss, learning_rate, ):
     try:
+        grad = x[0];
+        ground_truth = x[1];
+        rows, cols = w.shape;
+        for i in range(rows):
+            for j in range(cols):
+                # w[i][j] += learning_rate * loss * (-x[j][0]);
+                if(i == ground_truth):
+                    w[i][j] += learning_rate * loss * (grad[j][0]);
+                else:
+                    w[i][j] += learning_rate * loss * (-grad[j][0]);
+        with open(w_dmp_fname, 'wb') as f:
+            pickle.dump(w, f);
+        return w;
+    except Exception as e:
+        print("update(): %s" % e);
+        return None;
+
+def train(training_imgs, w, Lambda, learning_rate, ):
+    try:
+        YES = 0;
+        NO = 0;
+        random.shuffle(training_imgs);
         size = len(training_imgs);
         for i in range(size):
             score = np.dot(w, training_imgs[i][0]);
-            print(score, training_imgs[i][1]);
+            loss = total_loss(w, score, training_imgs[i][1], Lambda);
+            
+            maxx = -1;
+            idx = -1;
+            for j in range(score.shape[0]):
+                if(maxx < score[j][0]):
+                    maxx = score[j][0];
+                    idx = j;
+            # print("predicted:\t%d\nground truth:\t%d" % (idx, training_imgs[i][1]));
+            if(idx == training_imgs[i][1]):
+                YES += 1;
+            else:
+                NO += 1;
+            # print(YES, NO);
+            ratio = 100 * YES/(YES+NO);
+            print("trained %d(%d/%d) pics, precision: %.2f%%" % (YES + NO, YES, NO, ratio), end = '\r');
+            update(w, training_imgs[i], loss, learning_rate);
+            # for i in range(score.shape[0]):
+            #     print(score[i][0]);
+            # input();
         return w;
     except Exception as e:
         print("train(): %s" % e);
 
-train(training_imgs, w);
+
+Lambda = 0.02;
+learning_rate = 0.000000005;
+train(training_imgs, w, Lambda, learning_rate);
+
