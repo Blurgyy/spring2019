7bf899dcc830aed5ca9516b6d7556692477966dd
Blurgy <gaoyangzhang@outlook.com>
2019-06-25 18:59:44 +0800

    fix

--- DIP/mnist/exe/tester.py
+++ DIP/mnist/exe/tester.py
@@ -35,21 +35,20 @@ def test(testing_imgs, w, ):
     fn_name = "test";
     try:
         size = len(testing_imgs);
-        for i in range(size):
-            score = np.dot(w, testing_imgs[i][0]);
-            print(score);
-            input();
+        YES = 0;
+        NO = 0;
         for i in range(size):
             x = testing_imgs[i][0];
             gt = testing_imgs[i][1];
-            print(x.shape, w.shape);
             score = np.dot(w, x);
             idx = np.argmax(score);
             if(idx == gt):
                 YES += 1;
             else:
                 NO += 1;
+            ratio = YES / (YES + NO) * 100;
             print("\rtested \033[1;37m%d\033[0m(\033[1;32m%d\033[0m/\033[1;31m%d\033[0m) pics, precision: %.2f%%" % (YES + NO, YES, NO, ratio), end = '     ');
+        print();
     except Exception as e:
         print("%s(): %s" % (fn_name, e));
 
